54b18ebd80ce5284eb2dec77dc72ac58b2235b33
Govindu713 <pullagura.sai@s.amity.edu>
2026-07-21 19:22:26 +0530

    Added StudentDetails.java

---
+++ StudentDetails.java
@@ -0,0 +1,12 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        String name="Govindu Sai";
+        int age=20;
+        float cgpa =8.8f;
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("CGPA:"+cgpa);
+    }
+
+    
+}
