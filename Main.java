03b56e3244513954c7260b08adf668e8be97ea92
Govindu713 <pullagura.sai@s.amity.edu>
2026-07-22 19:43:46 +0530

    Added Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a double value::");
-        double value=sc.nextDouble();
-        System.out.println("The double value is:"+value);
-        int num=(int) value;
-        System.out.println("The integer value is:"+num);
+        System.out.println("Enter first number:");
+        int a=sc.nextInt();
+        System.out.println("Enter second number:");
+        int b=sc.nextInt();
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Subtraction:"+(a-b));
+        System.out.println("Multiplication:"+(a*b));
+        System.out.println("Division:"+(a/b));
+        System.out.println("Modulus:"+(a%b));
     }
 }
