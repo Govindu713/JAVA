5f56c2de0f9fde183fb866eccbbd8938179392a9
Govindu713 <pullagura.sai@s.amity.edu>
2026-07-28 20:13:23 +0530

    Added SwitchCalculator.java

---
+++ SwitchCalculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class SwitchCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second Number: ");
+        int b = sc.nextInt();
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.print("Enter Your Choice: ");
+        int choice = sc.nextInt();
+        switch(choice) {
+            case 1:
+                System.out.println("Addition = " + (a + b));
+                break;
+            case 2:
+                System.out.println("Subtraction = " + (a - b));
+                break;
+            case 3:
+                System.out.println("Multiplication = " + (a * b));
+                break;
+            case 4:
+                System.out.println("Division = " + (a / b));
+                break;
+            default:
+                System.out.println("Invalid Choice");
+        }
+    }
+}
