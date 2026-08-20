608063981cde8c6a0a0567835135ca2839d7d37e
Govindu713 <pullagura.sai@s.amity.edu>
2026-08-20 21:12:55 +0530

    Added CountOccurrence.java

---
+++ CountOccurrence.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class CountOccurrence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size of Array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter Array Elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter Element to Count: ");
+        int search = sc.nextInt();
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+
+            if(arr[i] == search) {
+                count++;
+            }
+        }
+        System.out.println(search + " occurs " + count + " times");
+    }
+}
