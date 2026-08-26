b467dff5080b6c988f1afe595a1ffee02d5a53ba
Govindu713 <pullagura.sai@s.amity.edu>
2026-08-26 21:21:34 +0530

    Added MissingNumber.java

---
+++ MissingNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class MissingNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n - 1];
+        System.out.println("Enter " + (n - 1) + " numbers:");
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int i = 0; i < n - 1; i++) {
+            actualSum = actualSum + arr[i];
+        }
+        int missing = expectedSum - actualSum;
+        System.out.println("Missing Number = " + missing);
+    }
+}
