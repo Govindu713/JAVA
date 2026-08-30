21414e44590ad4bb2516d2a715b86187c14aa22a
Govindu713 <pullagura.sai@s.amity.edu>
2026-08-30 20:45:46 +0530

    Added UniqueElements.java

---
+++ UniqueElements.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class UniqueElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Unique Elements:");
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
