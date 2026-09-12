c10462e032a8c3674386cd69d5fa884ca31c4650
Govindu713 <pullagura.sai@s.amity.edu>
2026-09-12 21:28:51 +0530

    Added RemoveDuplicate.java

---
+++ RemoveDuplicate.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int newArr[] = new int[n];
+        int newSize = 0;
+        for (int i = 0; i < n; i++) {
+            boolean duplicate = false;
+            for (int j = 0; j < newSize; j++) {
+                if (arr[i] == newArr[j]) {
+                    duplicate = true;
+                    break;
+                }
+            }
+            if (duplicate == false) {
+                newArr[newSize] = arr[i];
+                newSize++;
+            }
+        }
+        System.out.println("Array after removing duplicates:");
+        for (int i = 0; i < newSize; i++) {
+            System.out.print(newArr[i] + " ");
+        }
+    }
+}
