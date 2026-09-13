c58857bc3317e4933eca864d5e664be51965973b
Govindu713 <pullagura.sai@s.amity.edu>
2026-09-13 15:34:31 +0530

    Added Frequency.java

---
+++ Frequency.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Frequency {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean visited[] = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+
+            if (visited[i] == true) {
+                continue;
+            }
+
+            int count = 1;
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.println(arr[i] + " = " + count + " times");
+        }
+    }
+}
